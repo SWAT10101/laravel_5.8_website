f1f1659f6c7b992204443d4e07df35812e81aa63
swat10101 <swat1994911@gmail.com>
2019-04-04 00:52:51 +0300

    modfiy sider

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -7,12 +7,12 @@
         </a>
     </li>
     <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown"
+        <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" 
             aria-haspopup="true" aria-expanded="false">
             <i class="fas fa-fw fa-folder"></i>
             <span>Pages</span>
         </a>
-        <div class="dropdown-menu" aria-labelledby="pagesDropdown" id = "popper">
+        <div class="dropdown-menu" aria-labelledby="pagesDropdown">
             <h6 class="dropdown-header">Operations:</h6>
             <a class="dropdown-item" href="{{ route('register') }}">Add User </a>
             <a class="dropdown-item" href="add_coustomer.html">Add Customer</a>
