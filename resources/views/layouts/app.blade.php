81824ce4821da81588ba01354db69d1f71255df4
swat10101 <swat1994911@gmail.com>
2019-04-04 01:00:34 +0300

    tyr to fix dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('includes.csslib')
-    @include('includes.jslib')
+    
     
     
 
@@ -39,7 +39,7 @@
     </div>
 
 
-    
+    @include('includes.jslib')
 </body>
 
 </html>
