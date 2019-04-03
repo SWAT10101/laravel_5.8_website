8c2eb33346a76957fdf878822a85f29d14602e37
swat10101 <swat1994911@gmail.com>
2019-04-04 01:28:39 +0300

    inport js in heder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @include('includes.csslib')
+    @include('includes.jslib')
     
     
     
@@ -39,7 +40,7 @@
     </div>
 
 
-    @include('includes.jslib')
+    
 </body>
 
 </html>
