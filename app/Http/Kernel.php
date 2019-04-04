f6f7922c8215f122e18fe6ba4d1ed9847e9fc853
swat10101 <swat1994911@gmail.com>
2019-04-04 17:05:22 +0300

    try fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,   
+        \App\Http\Middleware\TrustProxies::class,  
+        \Barryvdh\Cors\HandleCors::class, 
         
                
     ];
@@ -42,7 +43,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            
+            \Barryvdh\Cors\HandleCors::class,            
         ],
     ];
 
@@ -63,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class
         
     ];
 
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', 'Auth\LoginController@showLoginForm');
 Auth::routes(['verify' => true]);
     
 
-    Route::get('/home', 'HomeController@index')->middleware('auth')->name('home');
+    Route::get('/home', 'HomeController@index')->middleware('auth', 'cors')->name('home');
 
   
     
